48d3ec28ef9255f4d7bb18894e046f3dd6596085
MateoParrado1 <mateo.parrado@ucb.edu.bo>
2026-01-08 12:25:22 -0400

    Add files via upload

---
+++ Primer codigo phyton.py
@@ -0,0 +1,24 @@
+print("Hello, World!")
+
+x=5
+y=10
+resultado= x + y 
+print("Hola, Mateo el resultado es : ")
+print(resultado)
+
+print("---------------------------")
+
+
+print("calcular el area de un rectangulo")
+base = 10
+altura = 57 
+area_rectangulo = base * altura
+print(area_rectangulo)
+
+
+print("---------------------------")
+
+
+print(7*8)
+
+
